36681c1d46f1a92920084553c068b194f8ab0113
Vanshika <0562vanshikasingh@gmail.com>
2025-03-30 02:17:42 +0530

    Fatch_data

--- my_project/src/App.jsx
+++ my_project/src/App.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Navbar from './components/Navbar'
 import './App.css'
 
 function App() {
@@ -18,6 +17,7 @@ function App() {
 
   return (
     <>
+    <Navbar></Navbar>
       <div className="container">
         {cards.map((card)=>{
           return <div key={card.id} className="card">
